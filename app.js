8ec8f9b5b095b86b03134145e7be5d90170eacd0
Stephen Mgbeojikwe <mgbeojikwestephen@gmail.com>
2020-02-19 18:18:42 +0100

    HTML CSS AND JS FILE

---
+++ app.js
@@ -0,0 +1,10 @@
+particlesJS.load('homeParticles-js','particles.json',function () {
+    console.log('cnaonfshvklc');
+    
+})
+
+particlesJS.load('foucsParticles-js','particles.json',function () {
+    console.log('cnaonfshvklc');
+    
+})
+
